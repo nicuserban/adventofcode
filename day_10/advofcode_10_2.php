73e997375d8d45b5c759f7f47a37fced7e8e1506
nicuserban <nicolae.serbandb@gmail.com>
2016-01-18 22:27:52 +0200

    Day 10 solutions

---
+++ day_10/advofcode_10_2.php
@@ -0,0 +1,36 @@
+<?php
+$input_str='1321131112';
+$turns_nr=50;
+
+echo '<pre>';
+var_dump(look_and_say($input_str, $turns_nr));
+echo '</pre>';
+
+function look_and_say($str, $turns_nr)
+{
+	for($i=0; $i<$turns_nr; $i++)
+	{
+		$length=strlen($str);
+		$new_str='';
+		$current_char_nr=0;
+		$current_char='';
+		for($j=0;$j<$length;$j++)
+		{
+			$char=$str[$j];
+			if(empty($current_char) || $current_char!=$char)
+			{
+				if(!empty($current_char) && !empty($current_char_nr))
+					$new_str.=$current_char_nr . $current_char;
+				$current_char=$char;
+				$current_char_nr=0;
+			}
+			$current_char_nr++;
+			$chars[$char]=true;
+		}
+		
+		$new_str.=$current_char_nr . $current_char;
+		$str=$new_str;
+		
+	}
+	return strlen($str);
+}
